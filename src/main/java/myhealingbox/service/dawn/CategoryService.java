050cd4d6687089fddae0f040202a1f31faa1a62b
boyjc <boyjc@DESKTOP-F6LHSDB>
2018-04-11 15:44:20 +0900

    2018-04-11 유노

--- src/main/java/myhealingbox/service/dawn/CategoryService.java
+++ src/main/java/myhealingbox/service/dawn/CategoryService.java
@@ -2,21 +2,51 @@ package myhealingbox.service.dawn;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import myhealingbox.dao.dawn.DawnCategoryDao;
 import myhealingbox.entity.dawn.DawnCategory;
 
 @Service
 public class CategoryService {
 
+	@Autowired
+	private DawnCategoryDao dawnCategoryDao;
+	
 	public List<DawnCategory> getCategoryList(Integer page) {
-		// TODO Auto-generated method stub
-		return null;
+
+		List<DawnCategory> categoryList = dawnCategoryDao.getList(page);
+		
+		return categoryList;
 	}
 
 	public DawnCategory getDawnCategory(Integer id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		DawnCategory dawnCategory = dawnCategoryDao.get(id);
+		
+		return dawnCategory;
+	}
+	
+	public int insertDawnCategory(DawnCategory dawnCategory) {
+		
+		int result = dawnCategoryDao.insert(dawnCategory);
+		
+		return result;
 	}
 
+	
+	public int updateDawnCategory(DawnCategory dawnCategory) {
+
+		int result = dawnCategoryDao.update(dawnCategory);
+
+		return result;
+	}
+
+	public int deleteDawnCategory(DawnCategory dawnCategory) {
+
+		int result = dawnCategoryDao.delete(dawnCategory);
+
+		return result;
+	}
 }
